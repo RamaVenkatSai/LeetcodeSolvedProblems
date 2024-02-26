a69d9339e2757d1e981d33767ac91e5519172fa2
RamaVenkatSai <62945493+RamaVenkatSai@users.noreply.github.com>
2024-02-26 00:36:32 -0500

    Time: 1575 ms (72.16%), Space: 71.2 MB (13.62%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,27 +1,49 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        graph = defaultdict(list)
+        secrets = set([0, firstPerson]) 
+        time_map={}
+        for src, dst, t in meetings:
+            if t not in time_map:
+                time_map[t] = defaultdict(list)
+            time_map[t][src].append(dst)
+            time_map[t][dst].append(src)
+        def dfs(src, adj):
+            if src in visit:
+                return
+            visit.add(src)
+            secrets.add(src)
+            for nei in adj[src]:
+                dfs(nei, adj)
+        for t in sorted(time_map.keys()):
+            visit = set()
+            for src in time_map[t]:
+                if src in secrets:
+                    dfs(src, time_map[t])
+        return list(secrets)
+      
+            
+#         graph = defaultdict(list)
 
-        for x,y,t in meetings:
-            graph[x].append((y,t))
-            graph[y].append((x,t))
+#         for x,y,t in meetings:
+#             graph[x].append((y,t))
+#             graph[y].append((x,t))
 
-        heap = [(0,firstPerson)] #time,person
-        for nei,time in graph[0]:
-            heapq.heappush(heap,(time,nei))
+#         heap = [(0,firstPerson)] #time,person
+#         for nei,time in graph[0]:
+#             heapq.heappush(heap,(time,nei))
         
-        visited = set([0])
+#         visited = set([0])
 
-        while heap:
-            time,person = heapq.heappop(heap)
+#         while heap:
+#             time,person = heapq.heappop(heap)
 
-            if person in visited:
-                continue
+#             if person in visited:
+#                 continue
 
-            visited.add(person)
+#             visited.add(person)
 
-            for nei,t in graph[person]:
-                if t>=time:
-                    heapq.heappush(heap,(t,nei))
-
-        return visited
+#             for nei,t in graph[person]:
+#                 if t>=time:
+#                     heapq.heappush(heap,(t,nei))
+# return visited
+    
