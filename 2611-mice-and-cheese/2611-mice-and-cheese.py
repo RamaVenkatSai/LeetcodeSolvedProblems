b307abd75f155da8c0ae7ab6d6ad20c51211cd41
RamaVenkatSai <62945493+RamaVenkatSai@users.noreply.github.com>
2024-03-06 01:20:09 -0500

    Time: 646 ms (93.09%), Space: 30.9 MB (93.09%) - LeetHub

--- 2611-mice-and-cheese/2611-mice-and-cheese.py
+++ 2611-mice-and-cheese/2611-mice-and-cheese.py
@@ -1,14 +1,17 @@
 class Solution:
     def miceAndCheese(self, reward1: List[int], reward2: List[int], k: int) -> int:
-        new_arr=[(reward1[i]-reward2[i],reward1[i] , reward2[i]) for i in range(len(reward2))]
-        new_arr.sort(reverse=True)
-        res = 0
-        for i in range(k):
-            res += new_arr[i][1]
+#         new_arr=[(reward1[i]-reward2[i],reward1[i] , reward2[i]) for i in range(len(reward2))]
+#         new_arr.sort(reverse=True)
+#         res = 0
+#         for i in range(k):
+#             res += new_arr[i][1]
 
-        for i in range(k , len(reward1)):
-            res += new_arr[i][2]
-        return res
+#         for i in range(k , len(reward1)):
+#             res += new_arr[i][2]
+#         return res
+          diff = [reward1[i] - reward2[i] for i in range(len(reward1))]
+        
+          return sum(reward2) + sum( sorted(diff, reverse = True)[:k])
 
         
             
