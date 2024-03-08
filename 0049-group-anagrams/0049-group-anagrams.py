fa88f829430e342b20f90b2640704e3634d570e0
RamaVenkatSai <62945493+RamaVenkatSai@users.noreply.github.com>
2024-03-08 17:03:56 -0500

    Time: 91 ms (50.18%), Space: 21.2 MB (29.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,22 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-
-
-        # Firstly here there are two things to remember
-        # Dictionary cannot have keys as mutable values such as list or another dictionary it can take only immutable datatypes i.e; tuple or string
-        # Here just we got the individualk count of 26 alphabets and joined them according to count of respective alphabets and printed the values
-        ll=collections.defaultdict(list)
-        
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def changer(i):
+            l=[0]*26
+            for j in i:
+                l[ord(j)-ord('a')]+=1
+            return tuple(l)
+        ll=defaultdict(list)
+        # print(ll)
         for i in strs:
-            counter=[0]*26
-            for m in i:
-
-                counter[ord(m)-ord('a')]=counter[ord(m)-ord('a')]+1
-            ll[str(counter)].append(str(i))
-        return ll.values()
+            str1=changer(i)
+            # print(str1)
+            if str1 in ll:
+                ll[str1].append(i)
+            else:
+                ll[str1]=[i]
+        return list(ll.values())
+            
+            
+        
+        
+        
