cac091cd826cfde04f5e1c49b5d515b10b921a6e
RamaVenkatSai <62945493+RamaVenkatSai@users.noreply.github.com>
2024-03-08 17:07:48 -0500

    Time: 125 ms (6.86%), Space: 20.2 MB (52.90%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def changer(i):
+        ll=defaultdict(list)
+        for i in strs:
             l=[0]*26
             for j in i:
                 l[ord(j)-ord('a')]+=1
-            return tuple(l)
-        ll=defaultdict(list)
-        # print(ll)
-        for i in strs:
-            str1=changer(i)
-            # print(str1)
-            if str1 in ll:
-                ll[str1].append(i)
-            else:
-                ll[str1]=[i]
+            ll[str(l)].append(i)
+            
         return list(ll.values())
             
             
