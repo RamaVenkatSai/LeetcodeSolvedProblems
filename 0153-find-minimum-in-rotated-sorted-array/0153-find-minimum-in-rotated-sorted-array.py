724b3b09bb82d144dffbf93d803d79824a42dee1
RamaVenkatSai <62945493+RamaVenkatSai@users.noreply.github.com>
2024-03-09 02:23:53 -0500

    Time: 52 ms (5.68%), Space: 16.8 MB (79.43%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,24 +1,24 @@
-class Solution(object):
-    def findMin(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        l, r = 0, len(nums)-1
-        
-        while l <= r:
-            
-            mid = (l + r)//2 #FYI: Right bit shift by 1 is equivalent to dividing by 2 and is more efficient.
-            
-            #If the subarr size is 1 or the sub arr isn't rotated then we instantly know the min value.
-            if nums[l] <= nums[r]:
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        def binary_search(nums, l, r):
+            print(l)
+            print(r)
+            print('...........')
+            mid=(l+r)//2
+            if nums[l]<=nums[r]:
+                print('............')
                 return nums[l]
-            #If the array is rotated, then we must find the inflection point in the rotated half
+                
             else:
-                #If the left half is rotated take it but include the middle b/c the min value could be mid itself
-                if nums[mid] < nums[l]:
-                    r = mid
-                #Take the right half, the inflection point cannot include mid
+                
+                if nums[mid]<nums[l]:
+                    r=mid
+                    return binary_search(nums, l, r)
                 else:
-                    l = mid+1
+                    l=mid+1
+                    return binary_search(nums,l,r)
+        return binary_search(nums,0,len(nums)-1)
+                
+                
             
+        
